9d03dc21f0e0568e7b185ff2b3a7e76cd424a395
Muharrem Sari <m_saritr@hotmail.com>
2021-02-07 00:44:10 +0300

    6 şubat

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,14 +11,30 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+
+        }
+
+        private static void categoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-            
-
-
-
         }
     }
 }
---
+++ dene/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace dene
+{
+    public class Class1
+    {
+    }
+}
