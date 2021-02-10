01d9b8b9c31956c175e4377c1fd708e55b32fcea
Muharrem Sari <m_saritr@hotmail.com>
2021-02-10 22:28:36 +0300

    10 şubat ilk ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Entities.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +18,16 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult("Ürün ismi en az 2 karakter olmalı");
+            }
+            _productDal.Add(product);
+            return new SuccessResult("Ürün eklendi");
+        }
+
         public List<Product> GetAll()
         {
             return _productDal.GetAll();
@@ -25,6 +36,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p=> p.CategoryId==id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductId==productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
